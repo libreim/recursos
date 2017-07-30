e54a958498b6ad1c711f8d15252eb8ab1b2649b0
David Charte <fdavidcl@protonmail.com>
2017-07-30 18:18:52 +0200

    Add link field, fix year

--- docs/assets/js/new.js
+++ docs/assets/js/new.js
@@ -3,16 +3,17 @@ var filename = function(title) {
   var time = new Date();
   var day = time.getDate();
   var month = time.getMonth() + 1;
-  var year = time.getYear();
+  var year = time.getFullYear();
   
   return year + "-" + month + "-" + day + "-" + title.toLowerCase().replace(/\s/g, "-") + ".md";
 }
 
 
-var yml = function(title, author, tag, content) {
+var yml = function(title, link, author, tag, content) {
   var template = `---
 title: ${title}
 author: ${author}
+link: ${link}
 tags:
 - ${tag}
 ---
@@ -30,7 +31,13 @@ var form = document.querySelector("#new_resource");
 form.onsubmit = function(event) {
   form.querySelector("[name='filename']").value = filename(form.querySelector("[name='title']").value);
   form.querySelector("[name='message']").value = "Añade recurso: " + form.querySelector("[name='title']").value;
-  form.querySelector("[name='value']").value = yml(form.querySelector("[name='title']").value, form.querySelector("[name='author']").value, form.querySelector("[name='tag']").value, form.querySelector("[name='content']").value);
+  form.querySelector("[name='value']").value = yml(
+    form.querySelector("[name='title']").value,
+    form.querySelector("[name='link']").value,
+    form.querySelector("[name='author']").value,
+    form.querySelector("[name='tag']").value,
+    form.querySelector("[name='content']").value
+  );
   
   return true;
 };
